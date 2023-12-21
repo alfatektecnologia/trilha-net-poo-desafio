8b032b093b4a853d7812f4e006a61b5dd2c6c379
Emanoel <oliveiraemanoel.br@gmail.com>
2023-12-20 22:01:51 -0300

    Added code to POO Smartphone challenge

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Nokia nokia = new Nokia("32 991234 9900","Nokia Xtreme","1234567890",32);
+
+Iphone iphone = new Iphone("84 991234 9900","Iphone 10","0987654321",128);
+
+nokia.Ligar();
+
+iphone.Ligar();
+
+nokia.ReceberLigacao();
+
+iphone.ReceberLigacao();
+
+nokia.InstalarAplicativo("Receita Federal");
+
+iphone.InstalarAplicativo("Onlyfans");
