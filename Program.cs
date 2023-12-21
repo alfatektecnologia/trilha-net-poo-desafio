0f14dcf0911c4114c80684c0a7a2612bef656114
Emanoel <oliveiraemanoel.br@gmail.com>
2023-12-20 22:32:12 -0300

    added changes to output format

--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-
-
+Console.Clear();
+Console.WriteLine("Testando o Smarphone NOKIA:");
 Nokia nokia = new Nokia("32 991234 9900","Nokia Xtreme","1234567890",32);
-
-Iphone iphone = new Iphone("84 991234 9900","Iphone 10","0987654321",128);
-
 nokia.Ligar();
-
-iphone.Ligar();
-
 nokia.ReceberLigacao();
-
-iphone.ReceberLigacao();
-
 nokia.InstalarAplicativo("Receita Federal");
 
+Console.WriteLine("\n");
+
+Console.WriteLine("Testando o Smarphone IPHONE:");
+Iphone iphone = new Iphone("84 991234 9900","Iphone 10","0987654321",128);
+iphone.Ligar();
+iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Onlyfans");
+
+Console.WriteLine("\n");
+Console.WriteLine("Final do desafio!");
+Console.WriteLine("\n");
